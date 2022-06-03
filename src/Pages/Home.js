00e3ce8223acd054eeac3d9ea2b41e3b866ad9e9
KHill96 <KHill0996@gmail.com>
2022-06-03 06:58:08 -0400

    Moved HomeContent component calls

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -14,9 +14,21 @@ export default function Home(){
     return(
         <div>
             {/* Books Component */}
-            <HomeContent items="books"/>
+            <h1 className="heading">Books</h1>
+            <h2 className="sub-heading">&emsp;&emsp;&emsp;Recently Added</h2>
+            <HomeContent items="books" category="recentlyAdded"/>
+            <h2 className="sub-heading">&emsp;&emsp;&emsp;Most Popular</h2>
+            <HomeContent items="books" category="mostPopular"/>
+            
+            
             {/* Movies Component */}
-            <HomeContent items="movies"/> 
+            <h1 className="heading">Movies</h1>
+            <h2 className="sub-heading">&emsp;&emsp;&emsp;Recently Added</h2>
+            <HomeContent items="movies" category="recentlyAdded"/>
+            <h2 className="sub-heading">&emsp;&emsp;&emsp;Most Popular</h2>
+            <HomeContent items="movies" category="mostPopular"/>
+            
+            
         </div>
     );
 
