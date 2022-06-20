725b21005a04b17f7d534ffb289ab6c7767a2039
KHill96 <KHill0996@gmail.com>
2022-06-20 06:52:02 -0400

    Spacing fixed

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -9,12 +9,8 @@ export default function About(){
 
     return (
         <div className="about">
-
-
-            <h3>About the Public Repository</h3>
-            
-            
-            <p> &emsp; The Public Repository is a personal project that stores and provides access to work in the public domain. As of right now work is limited to movies, but more material will come over time.</p>
+            <h3>About the Public Repository</h3> 
+             <p> &emsp; The Public Repository is a personal project that stores and provides access to work in the public domain. As of right now work is limited to movies, but more material will come over time.</p>
             <h3> Watch, Read, or Download</h3>
             <p> &emsp; Watch or read some of our movies or books in your browser, or download them to have handy at any time.</p>
             <h3>What's the Mission?</h3>
