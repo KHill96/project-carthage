84a3e62fb07dbbcacd976b36a75996104ca2483a
Kenneth Hill <khill0996@gmail.com>
2022-07-07 06:54:40 -0400

    Files created for additional pages

---
+++ src/Pages/Search.js
@@ -0,0 +1,9 @@
+import {React, useState, useEffect} from "react";
+import { useParams } from "react-router";
+
+const Search = () => {
+
+}
+
+
+export default Search;
---
+++ src/Pages/Theaters.js
@@ -0,0 +1,7 @@
+
+
+const Theaters = () =>{
+
+}
+
+export default Theaters;
