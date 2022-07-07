585e6c7952d91470cd6167b7306a1f6c20e96665
Kenneth Hill <khill0996@gmail.com>
2022-07-07 06:57:23 -0400

    Fixed a bug in Footer to keep it's position. Altered some of the HomeContent generation file

--- src/Components/HomeContent.js
+++ src/Components/HomeContent.js
@@ -7,6 +7,7 @@ export default function HomeContent (props){
     const listItemsRecentlyAdded = [];
     const listItemsMostPopular = [];
     const bookCoverTemp = "https://images-na.ssl-images-amazon.com/images/I/81X-7ubCi2L.jpg"
+    const movieCoverTemp = ""
     // Generate listItems
     var idString = "";
     var totalInList = 6;
@@ -25,7 +26,7 @@ export default function HomeContent (props){
             else{
                 listItemsMostPopular.push(
                     <li className="horizontal">
-                        <a className="title" href=".">
+                        <a className="title" href={idString}>
                             <img src={bookCoverTemp} alt="cover"/>
                             <p>BookText</p>
                         </a>
@@ -33,10 +34,11 @@ export default function HomeContent (props){
             }   
         }
         else{
+            idString = '/movies/' + i;
             if (props.category==="recentlyAdded"){
                 listItemsRecentlyAdded.push(
                 <li className="horizontal">
-                    <a className="title" href=".">
+                    <a className="title" href={idString}>
                         <img src={bookCoverTemp} alt="cover"/>
                         <p>MovieText</p>
                     </a>
@@ -45,7 +47,7 @@ export default function HomeContent (props){
             else{
                 listItemsMostPopular.push(
                     <li className="horizontal">
-                        <a className="title" href=".">
+                        <a className="title" href={idString}>
                             <img src={bookCoverTemp} alt="cover"/>
                             <p>MovieText</p>
                         </a>
@@ -57,32 +59,32 @@ export default function HomeContent (props){
    
 
    
-        if (props.category==="recentlyAdded"){
-            return(
-                <div className="body-content">
-                    <span className="content">
+    if (props.category==="recentlyAdded"){
+        return(
+            <div className="body-content">
+                <span className="content">
+                <div className="recentlyAdded">
+                    <ul className="display">
+                        {listItemsRecentlyAdded}
+                    </ul>
+                </div>  
+            </span>
+        </div>  
+        )
+    }
+    else{
+        return(
+            <div className="body-content">
+                <span className="content">
                     <div className="recentlyAdded">
                         <ul className="display">
-                            {listItemsRecentlyAdded}
+                            {listItemsMostPopular}
                         </ul>
                     </div>  
                 </span>
-            </div>  
-            )
-        }
-        else{
-            return(
-                <div className="body-content">
-                    <span className="content">
-                        <div className="recentlyAdded">
-                            <ul className="display">
-                                {listItemsMostPopular}
-                            </ul>
-                        </div>  
-                    </span>
-                </div> 
-            )
-        }
+            </div> 
+        )
+    }
     
 
    
