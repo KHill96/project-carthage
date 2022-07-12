846d4a669d454051763872d6d441b925f44a1bc6
Kenneth Hill <khill0996@gmail.com>
2022-07-12 10:25:01 -0400

    Changed placeholder text

--- client/src/Pages/Book.js
+++ client/src/Pages/Book.js
@@ -24,8 +24,7 @@ export default function Book(){
                     
 
                         <span className="description-links">
-                            BOok good lorem ipsum meme classic tale of alien invasion has to this day never been out of print. Like many works of the era, it was originally published as a serial—though the publisher, Pearson’s Magazine, demanded to know the ending before committing to publication.
-                                
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.                                
                         </span>
                        
                     </div>
