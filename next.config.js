0fe6bb385d2f7821978da9c8939d6d00fe12a1e3
Kenneth Hill <khill0996@gmail.com>
2022-07-26 07:48:15 -0400

    Added custom routes in config file

--- next.config.js
+++ next.config.js
@@ -6,6 +6,20 @@ const nextConfig = {
   images:{
     domains:['images-na.ssl-images-amazon.com', 'localhost'],
     formats: ['image/avif', 'image/webp'],
+  },
+
+  async redirects(){
+    return[{
+      source: '/books',
+      destination:'/books/popular',
+      permanent:true,
+    }
+    ,
+    {
+      source:'/movies',
+      destination:'/movies/popular',
+      permanent:true,
+    }]
   }
 }
 
