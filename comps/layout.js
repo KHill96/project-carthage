61698952aed6a5de1e9656f2435e3bf469e3cd10
Kenneth Hill <khill0996@gmail.com>
2022-07-26 07:42:28 -0400

    Removed footer from Layout

--- comps/layout.js
+++ comps/layout.js
@@ -1,12 +1,10 @@
 import Header from '../comps/header'
-import Footer from '../comps/footer'
 
 const Layout = ({children}) => {
     return ( 
         <div className='content'>
             <Header />
                 {children}
-            <Footer/>
         </div>
      );
 }
