0e336045b38458101bb70c24e73e11127dcf23f4
Kenneth Hill <khill0996@gmail.com>
2022-07-26 07:42:07 -0400

    Changed the reference for dropdowns and made the navbar sticky on top

--- comps/header.js
+++ comps/header.js
@@ -8,15 +8,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Header() {
   return (
-    <Navbar className="navbar" expand="lg" variant='dark'>
+    <Navbar className="navbar" expand="lg" variant='dark' sticky='top'>
       <Container fluid>
         <Navbar.Brand className="brand" href="/">Project Carthage</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" className='toggler'/>
         <Navbar.Collapse id="navbarScroll">
           <Nav className="me-auto my-2 my-lg-0">
             <NavDropdown id="top-item" title="Books" >
-              <NavDropdown.Item href="#" className='sub-item'>Popular</NavDropdown.Item>
-              <NavDropdown.Item href="#" className='sub-item'>New</NavDropdown.Item>
+              <NavDropdown.Item href="/books/popular" className='sub-item'>Popular</NavDropdown.Item>
+              <NavDropdown.Item href="/books/new" className='sub-item'>New</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Movies" id="top-item">
               <NavDropdown.Item href="#" className='sub-item'>Popular</NavDropdown.Item>
