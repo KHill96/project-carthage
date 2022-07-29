7253c66cc29b589751ab6747e51089b27bbe4e52
Kenneth Hill <khill0996@gmail.com>
2022-07-29 07:55:43 -0400

    Finished search result implementation. No pagination yet

--- pages/search/[term].js
+++ pages/search/[term].js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
 import axios from 'axios'
+import styles from '../../styles/Search.module.css'
 
 
 export async function getServerSideProps(context){
@@ -11,17 +12,35 @@ export async function getServerSideProps(context){
         let temp = await res.data
         results = temp.data
     }
-    else{
-        console.log("No results")
-    }
+    
     return{props:{results}}
 }
 
 
 const Search = ({results}) => {
     return(
-        <div>
-            {/* See Home Content on how to render */}
+        <div className={styles.body_content}>
+            
+            <ul className={styles.display}>
+                {results &&
+                    results.map((item,idx) => {
+                        return(
+                            <li className={styles.horizontal} key={idx}>
+                                <a className={styles.title} href={`/books/${item.id}`}>
+                                    <Image 
+                                    className={styles.cover_image}
+                                    src={item.cover_image_link}
+                                    alt='cover'
+                                    width='180px'
+                                    height='320px'
+                                    />
+                                    <p>{item.title}</p>
+                                </a>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </div>
     )
 
