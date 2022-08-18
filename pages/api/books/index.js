0e590dbba9b58f1402c0978335cb950a51a86ed4
Kenneth Hill <khill0996@gmail.com>
2022-08-17 21:07:04 -0400

    API now more aligned with being RESTful

--- pages/api/books/index.js
+++ pages/api/books/index.js
@@ -10,7 +10,38 @@ export default async function handler(req,res) {
   switch (method){
     case 'GET':
       try {
-        const books = await Book.find({});
+        var books = null;
+        if (req.query.limit){
+          // Get limit
+          if (req.query.sort){
+            // Popular
+            if(req.query.sort === 'popular'){
+              books = await Book.find().sort({total_downloads:-1}).limit(req.query.limit);
+            }
+            // Recent
+            else if(req.query.sort === 'recent'){
+              books = await Book.find().sort({date:-1}).limit(req.query.limit);
+            }
+            // Otherwise
+            else{
+              books = await Book.find().limit(req.query.limit)
+            }
+          }
+        }
+        else{
+          if(req.query.sort === 'popular'){
+            books = await Book.find().sort({total_downloads:-1});
+          }
+          // Recent
+          else if(req.query.sort === 'recent'){
+            books = await Book.find().sort({date:-1});
+          }
+          // Otherwise
+          else{
+            books = await Book.find();
+          }
+        }
+        // const recentBooks = await Book.find().sort({total_downloads:-1});
         res.status(200).json ({success: true,data: books})
       }catch(error){
         res.status(400).json({success:false})
