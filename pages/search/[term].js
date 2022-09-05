ee3e2164a7a73246b6c0939beeec8488d45c4a92
Kenneth Hill <khill0996@gmail.com>
2022-09-05 14:52:02 -0400

    Search page completed

--- pages/search/[term].js
+++ pages/search/[term].js
@@ -4,45 +4,53 @@ import Image from 'next/image'
 import axios from 'axios'
 import styles from '../../styles/Search.module.css'
 
-
 export async function getServerSideProps(context){
-    var results
     if(context.query.term){
+        var term = context.query.term;
         const res = await axios.get(`${process.env.SERVER_API_ENDPOINT}/api/search/${context.query.term}`)
-        let temp = await res.data
-        results = temp.data
+        var results = await res.data
     }
-    
-    return{props:{results}}
+    return {props:{results, term}}
 }
 
 
-const Search = ({results}) => {
-    return(
-        <div className={styles.body_content}>
-            
-            <ul className={styles.display}>
-                {results &&
-                    results.map((item,idx) => {
-                        return(
-                            <li className={styles.horizontal} key={idx}>
-                                <a className={styles.title} href={`/books/${item.id}`}>
-                                    <Image 
-                                    className={styles.cover_image}
-                                    src={item.cover_image_link}
-                                    alt='cover'
-                                    width='180px'
-                                    height='320px'
-                                    />
-                                    <p>{item.title}</p>
-                                </a>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-        </div>
-    )
+const Search = ({results,term}) => {
+    if(results.success === true){
+        return(
+            <div className={styles.body_content}> 
+                <Head><title>Search Results: {term}</title></Head>
+                <ul className={styles.display}>
+                    {
+                        results.data.map((item,idx) => {
+                            return(
+                                <li className={styles.horizontal} key={idx}>
+                                    <a className={styles.title} href={`/books/${item.id}`}>
+                                        <Image 
+                                        className={styles.cover_image}
+                                        src={item.cover_image_link}
+                                        alt='cover'
+                                        width='180px'
+                                        height='320px'
+                                        />
+                                        <p>{item.title}</p>
+                                    </a>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        )
+    }
+       
+    else{
+        return(
+            <div className={styles.body_content}>
+                <Head><title>No Results: {term}</title></Head>
+                <h1 className={styles.no_results}>No Results Found</h1>
+            </div>
+        )
+    }
 
 }
 
