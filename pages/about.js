c8b016ca6e783ba6b6966095955f7d7b1da2a1c5
Kenneth Hill <khill0996@gmail.com>
2022-09-05 14:49:47 -0400

    Fixed typos

--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,21 @@
 const About = () => {
     return(
         
-        <div>
+        <div className="about">
             <h3>About the Project Carthage</h3> 
             <p> &emsp; Project Carthage is a project that stores and provides access to work in the public domain.</p>
             <h3>Sources</h3> 
-            <p> &emsp; Books are gathered from several different sources. PDF`&apos;` and some EPUB`&apos;`s are taken from Google, with some PDF`&apos;`s taken from other educational sites. 
-            Webpages are from <a href="www.gutenberg.org">Project Gutenberg</a>. EPUB and AZW3 formats are mainly taken from <a>Standard Ebooks</a>. 
+            <p> &emsp; Books are gathered from several different sources. PDF&apos;s and some EPUB&apos;s are taken from Google, with some PDF&apos;s taken from other educational sites. 
+            Webpages are from <a href="https://www.gutenberg.org">Project Gutenberg</a>. EPUB and AZW3 formats are mainly taken from <a href="https://www.standardebooks.org">Standard Ebooks</a>. 
             </p>            
             
             <h3> Read and Download</h3>
             <p> &emsp; Read some of our books in your browser, or download them to have on hand at any time.</p>
             <p> &emsp; Work in the public domain should be easily accessible for all. Everyone should be encouraged to explore content in the public domain and to utilize the resources that make that content available. </p>
             <h3>Banned Books</h3>
-            <p> &emsp; This site is based in an area where several books have not been banned. If you are located in an area where access to certain books has been blocked or revoked, you are encouraged to find those materials here. </p>
+            <p> &emsp; This site is based in an area where several books have not been banned, contrary to other states&apos; policies. If you are located in an area where access to certain books has been blocked or revoked, you are encouraged to find those materials here. </p>
             <p className="disclaimer">
-            <b>The site follows US Copyright Law and it`&apos;` definition of work public domain.</b></p>
+            <b>The site follows US Copyright Law and it&apos;s definition of work public domain.</b></p>
         </div>
     );
 }
